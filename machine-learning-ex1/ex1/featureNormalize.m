3ec2e81a31ab20d4078ea8f9dc24c07d7e4dbb07
wangandai <wang.andai@gmail.com>
2017-06-20 18:24:37 +0800

    Updated ex1

--- machine-learning-ex1/ex1/featureNormalize.m
+++ machine-learning-ex1/ex1/featureNormalize.m
@@ -25,15 +25,10 @@ sigma = zeros(1, size(X, 2));
 %
 % Hint: You might find the 'mean' and 'std' functions useful.
 %       
-
-
-
-
-
-
-
-
-
+mu = mean(X_norm)
+sigma = std(X_norm)
+X_norm = (X_norm-mu)./sigma
+% X_norm = [ones(size(X_norm),1), X_norm]
 % ============================================================
 
 end
--- machine-learning-ex1/ex1/gradientDescentMulti.m
+++ machine-learning-ex1/ex1/gradientDescentMulti.m
@@ -16,17 +16,9 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
-
-
-
-
-
-
-
-
-
-
-
+	h = X*theta
+	u = sum((h-y).*X)
+	theta = theta - (alpha*u/m)'
     % ============================================================
 
     % Save the cost J in every iteration    
